cb3b2cc3715d5ef4a41a8ebdd9d32950e9756275
PYTH@N <python911@protonmail.com>
2020-02-12 12:40:57 +0530

    Create win7-do.sh

---
+++ cloud/win7-do.sh
@@ -0,0 +1,16 @@
+For Windows 10:
+
+wget -O- https://www.dropbox.com/s/e2oglmh3tdu3boz/WIn10.gz?dl=1 | gunzip | dd of=/dev/vda
+
+For Windows 2012:
+
+wget -O- https://www.dropbox.com/s/eiductyclyf7kve/WinServerImageDO.gz?dl=1 | gunzip | dd of=/dev/vda
+
+9- After the command finishes, turn off the machine and set it back to boot from hard disk in the recovery section in the digital ocean droplet management dashboard.
+
+10. Turn on the droplet.
+
+11 – Login with the following user and password:
+
+username: WhatUpTime.com
+Password: P@ssword64
